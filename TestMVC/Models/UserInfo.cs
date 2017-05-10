2ce94b2f347d8d957ebd4b7d23d72b822eac94dc
BrettMerri <the.b.merri@gmail.com>
2017-05-10 10:16:10 -0400

    initial commit

---
+++ TestMVC/Models/UserInfo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC.Models
+{
+    public class UserInfo
+    {
+        private string firstName;
+        private string lastName;
+        private string username;
+        private string email;
+        private string password;
+
+        public UserInfo() : this("", "", "", "", "") { }
+
+        public UserInfo(string firstName, string lastName, string username, string email, string password)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+    }
+}
